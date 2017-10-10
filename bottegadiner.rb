a027d9c6df8aa9c16846f95c5eaa95930ae285e1
Tania D'Angelo <taniadangelo@Tanias-MacBook-Pro.local>
2017-10-10 11:19:07 -0500

    Added exercises to existing directory

---
+++ bottegadiner.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+
+entire_menu = {
+  "Breakfast($5.50)" => { 
+    "Choice of 1 entree" => ["pancakes", "eggs", "french toast"],     
+    "Choice of 2 sides" => ["hashbrowns", "fruit", "toast"]
+  },
+  "Lunch($8.00)" => {
+    "Choice of 1 entree" => ["chicken", "steak", "fish"],
+    "Choice of 2 sides" => ["steamed vegetables", "baked potato", "french fries"]
+  },
+  "Dinner($12.50)" => {
+    "Choice of 1 entree" => ["chicken", "steak", "fish"],
+    "Choice of 2 sides" => ["steamed vegetables", "baked potato", "french fries"]
+  }
+}
+
+
+entire_menu.each do |x|
+puts x
+end
+
+puts "Would you like the Chef's Special?"
+answer = gets.chomp
+if answer == 'yes'
+  puts "Great Choice!"
+elsif answer =='no'
+  puts "What would you like instead?"
+end
+  
--- wk1d1.rb
+++ wk1d1.rb
@@ -1,5 +1,13 @@
 require 'rspec'
 
+str = "dog lazy the over jumped fox brown quick The"
+
+def sentence_reverser str
+  str.split.reverse.join(' ')
+end
+
+p sentence_reverser(str)
+
 def sentence_reverser str
   str.split.reverse.join(' ')
 end
