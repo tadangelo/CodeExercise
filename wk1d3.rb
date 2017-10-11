8a46ad8d606a7ed436d4ab61c3f1922cd8f29a9a
Tania D'Angelo <taniadangelo@Tanias-MacBook-Pro.local>
2017-10-11 11:14:26 -0500

    update

--- wk1d3.rb
+++ wk1d3.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 
-def array_converter *array
-  array.flatten
+def array_converter *arrays
+  arrays.flatten
 end
 
 arr_1 = ['1', '5', '9']
@@ -11,8 +11,8 @@ arr_3 = ['3', '4', '0']
 array_converter = [(arr_1), (arr_2), (arr_3)]
 puts array_converter
 
-def array_converter *array
-  array.map(&:inspect).join(', ')
+def array_converter *arrays
+  arrays.map(&:inspect).join(', ')
 end
 
 arr_1 = ['1', '5', '9']
