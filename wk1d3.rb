ef6dd24bd805f75bfebf111877bbb223b8f66dfb
Tania D'Angelo <taniadangelo@Tanias-MacBook-Pro.local>
2017-10-11 10:58:29 -0500

    multiple responses added for exercise 3 week 1

--- wk1d3.rb
+++ wk1d3.rb
@@ -1,5 +1,27 @@
 require 'rspec'
 
+def array_converter *array
+  array.flatten
+end
+
+arr_1 = ['1', '5', '9']
+arr_2 = ['10', '2', '7', '10']
+arr_3 = ['3', '4', '0']
+
+array_converter = [(arr_1), (arr_2), (arr_3)]
+puts array_converter
+
+def array_converter *array
+  array.map(&:inspect).join(', ')
+end
+
+arr_1 = ['1', '5', '9']
+arr_2 = ['10', '2', '7', '10']
+arr_3 = ['3', '4', '0']
+
+array_converter = [(arr_1), (arr_2), (arr_3)]
+puts array_converter
+
 def array_converter *arrays
   arrays.flatten.map(&:to_i)
 end
@@ -9,3 +31,5 @@ arr_2 = ['10', '2', '7', '10']
 arr_3 = ['3', '4', '0']
 
 puts array_converter arr_1, arr_2, arr_3
+
+
