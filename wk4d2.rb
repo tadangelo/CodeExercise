010e8be42999aa01c3ea26b4f264623601003863
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-01 11:32:01 -0500

    added wk4d2 code exercise

---
+++ wk4d2.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+
+def increment_value str
+str + str.next.slice(-1)
+end
+
+str = '123'
+p increment_value str
+str = 'abcde'
+p increment_value str
+
+describe 'Increment string value sequence' do
+  it 'appends the next item in the sequence of a string' do
+    expect(increment_value 'abcde').to eq('abcdef')
+    expect(increment_value '123').to eq('1234')
+  end
+end
