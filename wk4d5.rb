2db07aaa213a5de6bdfc872fc8a7f0729612056f
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-08 11:04:39 -0600

    Completed code exercise wk4d5

---
+++ wk4d5.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+
+def fraction_calculator fraction_one, fraction_two, operator
+  num_one = Rational(fraction_one)
+  num_two = Rational(fraction_two)
+
+  final_result = case operator
+  when '/' then num_one / num_two
+  when '*' then num_one * num_two
+  when '+' then num_one + num_two
+  when '-' then num_one - num_two
+end
+
+  String(final_result)
+end
+
+
+p fraction_calculator "1/2", "1/2", "*"
+p fraction_calculator "1/3", "2/4", "*"
+
+describe 'Fraction multiplication' do
+  it 'can multiply two fractions and output the value in a string based fraction format' do
+    expect(fraction_calculator("1/3", "2/4", "*")).to eq("1/6")
+    expect(fraction_calculator("1/3", "2/4", "/")).to eq("2/3")
+    expect(fraction_calculator("1/3", "2/4", "+")).to eq("5/6")
+    expect(fraction_calculator("1/3", "2/4", "-")).to eq("-1/6")
+  end
+end
