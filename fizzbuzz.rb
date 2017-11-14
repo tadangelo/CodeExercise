c00857b8d3cbedbc8d08d034d9b745cf727fe05a
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-14 12:47:28 -0600

    Diff solutions for fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,35 @@
+(1..100).each do |number|
+  if number % 5 == 0 && number % 3 == 0
+    puts 'FizzBuzz'
+  elsif number % 5 == 0
+    puts 'Buzz'
+  elsif number % 3 == 0
+    puts 'Fizz'
+  else
+    puts number
+  end
+end
+
+1.upto(100) do |num|
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  else
+    puts num
+  end
+end
+
+1.upto(100) do |num|
+  if num % 15 == 0
+    puts "FizzBuzz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  else
+    puts num
+  end
+end
